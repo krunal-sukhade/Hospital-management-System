ea131c8289dea74eb4318b46320fd9849487930f
Krunal Sukhade <krunalsukhade13@gmail.com>
2025-05-11 15:07:47 +0530

    Initial commit

---
+++ frontend/src/page/HomePage.jsx
@@ -0,0 +1,13 @@
+import Navbar from '../components/Shared/Navbar'
+import Home from '../components/Patient/Home'
+function HomePage() {
+  return (
+    <>
+        <Navbar/>
+        <Home/>
+        {/* <AdminDashboard/> */}
+    </>
+  )
+}
+
+export default HomePage
