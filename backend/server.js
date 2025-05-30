865d94a8735d06146b1c505ba1b9240cde826484
Krunal Sukhade <krunalsukhade13@gmail.com>
2025-05-30 12:25:32 +0530

    Fix CORS and cookie issues for login

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,8 @@ const limiter = require("./middlewares/rateLimiter");
 const router = express.Router();
 const app = express();
 
+
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(corsMiddleware);
